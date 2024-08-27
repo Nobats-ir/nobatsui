dd5624a9bb80eb1f1c66708e99e2e14afa8ef701
Javid Momeni <j.momeni@parsa.ir>
2024-08-27 11:35:29 +0330

    feat: github pages publish system

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,27 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [
+            'react',
+            'react-dom',
+            'react-router-dom',
+          ],
+          app: ['./src/main.tsx', './src/App.tsx'],
+          framework: [
+            '@mantine/core',
+            '@mantine/hooks',
+          ],
+        },
+      },
+    },
+    outDir: './docs',
+    chunkSizeWarningLimit: 600,
+  },
+  base: './',
   test: {
     globals: true,
     environment: 'jsdom',
